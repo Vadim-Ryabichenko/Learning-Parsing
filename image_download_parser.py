c7d8aa1d5ffa3ec13534c39b22bf553df6b02906
Vadim <vadimrembyboy@gmail.com>
2024-03-13 17:29:18 +0200

    download image parser simple update

--- image_download_parser.py
+++ image_download_parser.py
@@ -5,22 +5,32 @@ import fake_useragent
 
 my_user = fake_useragent.UserAgent().random
 header = {'user-agent': my_user}
-url = f'https://wallpaperscraft.ru/'
+url = f'https://www.nastol.com.ua/peoples/'
 
 response = requests.get(url, headers=header)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-block = soup.find('ul', class_='wallpapers__list')
-all_image = block.findAll('li', class_='wallpapers__item')
+
+block = soup.findAll('div', class_='verh')
+
 
 image_number = 1
 
-for image in all_image:
+for image in block:
     image_link = image.find('a').get('href')
-    print(image_link)
-    image_bytes = requests.get(f'{url}{image_link}').content
+    download = requests.get(image_link).text
+    download_soup = BeautifulSoup(download, 'html.parser')
+    download_block = download_soup.find('div', class_='llink')
 
-    with open(f'image/{image_number}.jpeg', 'wb') as file:
+    image_link2 = download_block.find('a').get('href')
+    download2 = requests.get(image_link2).text
+    download_soup2 = BeautifulSoup(download2, 'html.parser')
+    
+    result_link = download_soup2.findAll('div')[16].find('a').get('href')
+
+    image_bytes = requests.get(f'{result_link}').content
+
+    with open(f'image/{image_number}.jpg', 'wb') as file:
         file.write(image_bytes)
     image_number += 1
 
